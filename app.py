86dd1ea2cc5bb62a94294d4a46d474d84812d61a
Toitles123 <jiangjeric@gmail.com>
2024-03-27 14:43:32 -0400

    Added index.html

--- app.py
+++ app.py
@@ -33,8 +33,12 @@ def generate_token():
 #endregion
 
 #region Comments
-@app.route('/comments', methods=['POST', 'GET'])
+@app.route('/')
 def index():
+    return render_template('index.html')
+
+@app.route('/comments', methods=['POST', 'GET'])
+def comments():
     channel_id = 1
     
     try: 
